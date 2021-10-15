8453d41cbd18657a70d532f5075fb57103e6ee1b
MikkelMed <mafl20@student.aau.dk>
2021-10-15 13:34:43 +0200

    added some comments to test

--- Scripts/test.py
+++ Scripts/test.py
@@ -3,20 +3,21 @@ import cv2
 from TestClass import TestClass
 
 def main_function():
-    victor = TestClass()
+    victor = TestClass()    #we make an instance of TestClass, and call it victor
 
-    victor.martin_er_sej("amogus")
+    victor.martin_er_sej("amogus")  #we call the martin_er_sej()-method from the TestClass
 
+    # > This code is run until 'return False' or a 'break'
     while True:
-        user_input = cv2.waitKey(0)
+        user_input = cv2.waitKey(0) #we make a variable holding any user input
 
-        image = cv2.imread('images\\linus_sad_tips.png', 0)
+        image = cv2.imread('images\\linus_sad_tips.png', 0) #an image is stored
 
-        cv2.imshow("This is sad linus", image)
+        cv2.imshow("This is sad linus", image)  #the image is shown
 
         if user_input == 27:    #27 is the ascii code for the <ESC> key
-            break
+            break   #this breakes the while loop
 
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows() #all windows are closed when this is reached
 
 main_function()
