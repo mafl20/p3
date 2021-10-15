940b31bdf2bb62b33de699eae1c5711b093e35d9
VictorHjort <vhjort20@student.aau.dk>
2021-10-15 11:07:43 +0200

    Eyy

--- Scripts/victor.py
+++ Scripts/victor.py
@@ -1,1 +1,12 @@
-print('Victor rules')
+import cv2
+
+print('hello martin')
+
+imageName = "DX-9.JPG"
+
+image = cv2.imread(imageName, -1)
+
+cv2.imshow("MyImage",image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
