0045819df480f6961ab019177e9395518e8d73c8
MikkelMed <mafl20@student.aau.dk>
2021-10-15 11:07:15 +0200

    fuck victor
    møsmøs

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,8 +1,17 @@
 import cv2
 
-def main_function(message):
-    print(message)
+def main_function():
+    while True:
+        user_input = cv2.waitKey(0)
 
+        image = cv2.imread('images\\linus_sad_tips.png', 0)
+
+        cv2.imshow("This is sad linus", image)
+
+        if user_input == 27:    #27 is the ascii code for the <ESC> key
+            break
+
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main_function("Heelo Group 301!")
+    main_function()
